0018a83cbd613a9f940f80bcf9997fb043a7df45
Glen1337 <36278632+Glen1337@users.noreply.github.com>
2018-04-11 18:29:07 -0400

    Add files via upload

---
+++ RegexTester/RegexTester.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
+
+namespace Misc
+{
+    namespace Projects
+    {
+        class _Main
+        {
+            static void Main(string[] args)
+            {
+                RegexTester rt = new RegexTester(@"^ab\sc$");
+                rt.RunTests();
+            }
+        }
+
+        class RegexTester
+        {
+            List<String> tests = new List<String>
+            {
+                // Add text tests to match to regex here
+                @"ab c",
+                @"abc",
+                @" ab c "
+            };
+
+            private readonly Regex MainRegex;
+
+            // Ctor
+            public RegexTester(String inRegex)
+            {
+                MainRegex = new Regex(inRegex);
+            }
+            
+            public void RunTests()
+            {
+                Parallel.ForEach(tests,
+                                 delegate(String currentText)
+                                 {
+                                     if (MainRegex.IsMatch(currentText))
+                                     {
+                                         System.Diagnostics.Debug.WriteLine("Regex Matches: " + MainRegex.Match(currentText).Value + " in: " + currentText);
+                                     }
+                                     else
+                                     {
+                                         System.Diagnostics.Debug.WriteLine("No Match for: " + currentText);
+                                     }
+                                 });
+            }
+        }
+
+    }
+}

9900e830336203ec22ed569def2654aa29379a6e
Glen1337 <36278632+Glen1337@users.noreply.github.com>
2018-04-13 15:12:32 +0000

    Update start.bat

--- DMon/start.bat
+++ DMon/start.bat
@@ -1,7 +1,6 @@
 @echo off
 cd "C:\Users\1115027\Documents\Visual Studio 2013\Projects\Projects\DMon\bin\Release\"
 start DMon.exe "Z:\Glen_Anderson" "80" "N" "6"
-start DMon.exe "Z:\Sean_Duggan" "80" "N" "6"
 cd "C:\Users\1115027\Documents\Visual Studio 2013\Projects\Projects\Time\bin\Release\"
 REM needs text file on desktop called times.txt
 start Time.exe

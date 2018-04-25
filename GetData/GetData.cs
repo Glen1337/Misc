2c9473f513b2a429c9e1602691852704ee12ad3f
Glen1337 <36278632+Glen1337@users.noreply.github.com>
2018-04-25 18:03:28 -0400

    Add files via upload

--- GetData/GetData.cs
+++ GetData/GetData.cs
@@ -1,23 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Utilities;
 
 
 namespace Misc
 {
     namespace Projects
     {
+        public class StockInfo
+        {
+            public String open { get; set; }
+        }
+
+        public class Response
+        {
+            List<StockInfo> StockInfoList { get; set; }
+        }
+
+
 
         public class InfoCollector
         {
             static HttpClient client;
 
-            public void Setup()
+            public void ClientSetup()
             {
                 client = new HttpClient()
                 {
@@ -30,14 +49,23 @@ namespace Misc
 
             public async Task<HttpResponseMessage> GetResponseFromRequest(String args)
             {
-                return await client.GetAsync(args);
+                return await client.GetAsync(WebUtility.UrlEncode(args));
             }
 
-            public async Task<String> GetDataFromResponse(HttpResponseMessage response)
+            public async Task<String> GetStringFromResponse(HttpResponseMessage response)
             {
                 if (response.IsSuccessStatusCode)
                 {
                     return await response.Content.ReadAsStringAsync();
+                }
+                else { throw new HttpRequestException("Bad response from request"); }
+            }
+
+            public async Task<Stream> GetStreamFromResponse(HttpResponseMessage response)
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStreamAsync();
                 }
                 else { throw new HttpRequestException("Bad response from request"); }
             }
@@ -49,9 +77,42 @@ namespace Misc
             static void Main(string[] args)
             {
                 InfoCollector collector = new InfoCollector();
-                collector.Setup();
-                HttpResponseMessage r = collector.GetResponseFromRequest("stock/aapl/chart").Result;
+                collector.ClientSetup();
                 
+                HttpResponseMessage response = collector.GetResponseFromRequest("stock/aapl/chart").Result;
+
+                //using (Stream JsonStream = collector.GetStreamFromResponse(response).Result)
+                //{
+                //    JsonStream.
+                //}
+
+                String JsonString = collector.GetStringFromResponse(response).Result;
+
+                List<StockInfo> si = JsonConvert.DeserializeObject<List<StockInfo>>(JsonString);
+
+                JArray jsonObjectArray = JArray.Parse(JsonString);
+
+                //https://stackoverflow.com/questions/15726197/parsing-a-json-array-using-json-net
+                foreach(JObject jsonObject in jsonObjectArray.Children<JObject>())
+                {
+                    foreach (JProperty jsonProperty in jsonObject.Properties())
+                    {
+                        String name = jsonProperty.Name;
+                        String value = (String)jsonProperty.Value;
+                    }
+                }
+
+                //https://stackoverflow.com/questions/12676746/parse-json-string-in-c-sharp
+                //foreach (JObject jsonObject in jsonObjectArray)
+                //{
+                //    foreach (KeyValuePair<String, JToken> obj in jsonObject)
+                //    {
+                //        //var Key = obj.Key;
+                //        var close = (String )obj.Value["close"];
+                //    }
+                //}
+
+                int i = 0;
             }
         }
     }
